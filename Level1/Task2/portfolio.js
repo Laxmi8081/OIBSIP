1dc9622dd6804a273a7c2398aaeb16e9b24a1df4
Laxmi Bharti <108523034+Laxmi8081@users.noreply.github.com>
2023-03-29 21:08:30 +0530

    Update portfolio.js

--- Level1/Task2/portfolio.js
+++ Level1/Task2/portfolio.js
@@ -6,27 +6,5 @@ $(document).ready(function(){
     $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
-
-        // if($(window).scrollTop()>0){
-        //     $('.')
-        }
     });
 
-//  // smooth scrolling
-
-//     $('a[href*="#"]').on(click(function(e){
-        
-//     e,preventDefault();
-
-//     $('html,body').animate({
-
-//         scrollTop: $($)(this).attr('href').offset().top,
-
-//     },
-//     500,
-//     'linear'
-//     );
-
-//     });
-
-// });
